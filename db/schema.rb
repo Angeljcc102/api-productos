b21e3ed22bcfd618a90ef9d311213d5d8dd5d7a9
Angeljcc102 <angeljcc102@live.com.mx>
2021-09-23 14:28:51 -0500

    Agregado los modelos de Categoria y Producto
    Modelos realizados con scaffold, tambien se agrego las relaciones entre producto y categoria.

---
+++ db/schema.rb
@@ -0,0 +1,33 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_23_192302) do
+
+  create_table "categoria", force: :cascade do |t|
+    t.string "nombre", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "descripcion"
+    t.string "#<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x000001f5c8a239f0>"
+  end
+
+  create_table "productos", force: :cascade do |t|
+    t.string "nombre", null: false
+    t.string "descripcion", null: false
+    t.integer "categoria_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["categoria_id"], name: "index_productos_on_categoria_id"
+  end
+
+  add_foreign_key "productos", "categoria", column: "categoria_id"
+end
